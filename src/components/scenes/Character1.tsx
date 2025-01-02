3bf37d3921a3af0ae20a88ac7436c5fe32f7f911
TEIKOU2023 <zhenghengteikou@gmail.com>
2025-01-02 13:55:17 +0900

    Fix unused React import issue

--- src/components/scenes/Character1.tsx
+++ src/components/scenes/Character1.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import  { useState, useRef } from "react";
 import { useLoader, useFrame } from "@react-three/fiber";
 import * as THREE from "three";
 import DialogueBox from "../DialogueBox";
